87bfb39eb2382da55661601f258e00bd0bf5a12d
Mr-funkinguy <89234368+Mr-funkinguy@users.noreply.github.com>
2022-08-12 23:57:38 -0700

    Create start.js

---
+++ start.js
@@ -0,0 +1,19 @@
+//server = -9
+function start() {
+  ping 1.3.7.4.000
+  L/9_ping
+  Local.user.connect
+}
+
+start();
+
+https://api.cloudflare.com/client/v4/
+LocalStorage.save();
+
+cloudfareCheck() {
+    curl -X GET "https://api.cloudflare.com/client/v4/zones/cd7d0123e3012345da9420df9514dad0" \
+     -H "Content-Type:application/json" \
+     -H "X-Auth-User-Service-Key:v1.0-e24fd090c02efcfecb4de8f4ff246fd5c75b48946fdf0ce26c59f91d0d90797b-cfa33fe60e8e34073c149323454383fc9005d25c9b4c502c2f063457ef65322eade065975001a0b4b4c591c5e1bd36a6e8f7e2d4fa8a9ec01c64c041e99530c2-07b9efe0acd78c82c8d9c690aacb8656d81c369246d7f996a205fe3c18e9254a"`
+}
+
+4.4.8.8

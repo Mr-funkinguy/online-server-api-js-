09831c0ad6e50ff7898de571291d033940ea80b8
Mr-funkinguy <89234368+Mr-funkinguy@users.noreply.github.com>
2022-08-13 00:09:01 -0700

    Create multiplayer.js

---
+++ multiplayer.js
@@ -0,0 +1,11 @@
+If Server.Users = true
+  server.request = port('908');
+  .callback
+  ping 7965.2.4
+  
+
+user.get("1.08.3")
+Username.Cache('LocalServerStorage', 1.95.8)
+|
+/_02.4.8.5/36\\
+se{8}.,v':AS

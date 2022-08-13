4763b104261063d98dcc26eace04f5b97f07e272
Mr-funkinguy <89234368+Mr-funkinguy@users.noreply.github.com>
2022-08-13 00:01:46 -0700

    Create name.js

---
+++ name.js
@@ -0,0 +1,17 @@
+Users.currecnt
+Local.Username = (string);
+
+String.apply
+
+Function setuser() {
+  if user.Username = false
+  prompt("Set A Username")
+  //textbox code
+  If Confirm
+   get string('' user); //textbox id
+    User.setUser = string;
+  if User.Username = true
+    start.js
+    destroy();
+  
+}
